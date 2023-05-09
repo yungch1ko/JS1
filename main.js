f0376a0b62485a34978f467ab5d217841ada1595
yungch1ko <chikochikomba@gmail.com>
2023-05-09 09:01:31 +0200

    first com

---
+++ main.js
@@ -0,0 +1,10 @@
+let firstNumber = 5;
+let secondNumber = 10;
+
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+let result = sum(firstNumber, secondNumber);
+
+document.getElementById("result").innerHTML = result;
